29787b0b66463bad651ede44288138a654f7bbfa
John Tennant <jtennant@squareup.com>
2026-07-27 15:15:35 -0400

    Fix TTS stretcher pre-roll compensation
    Signed-off-by: John Tennant <jtennant@squareup.com>

--- desktop/src-tauri/src/huddle/tts.rs
+++ desktop/src-tauri/src/huddle/tts.rs
@@ -47,7 +47,7 @@ use std::{
     time::Duration,
 };
 
-use super::playback_speed::{PlaybackSpeedControl, PlaybackSpeedProcessor};
+use super::playback_speed::{process_complete_chunk, PlaybackSpeedControl};
 use super::pocket::{load_text_to_speech, load_voice_style, SAMPLE_RATE, VOICE_FILE_EXT};
 use super::preprocessing::{preprocess_for_tts, split_sentences};
 
@@ -652,9 +652,7 @@ fn tts_worker(config: TtsWorkerConfig, text_rx: mpsc::Receiver<QueuedText>) {
                     let buf =
                         build_sentence_append_buffer(&mut first_append, audio, silence_buf_len);
                     let speed = playback_speed.get();
-                    let buf = match PlaybackSpeedProcessor::new(speed, SAMPLE_RATE)
-                        .and_then(|mut processor| processor.process_complete_chunk(&buf))
-                    {
+                    let buf = match process_complete_chunk(&buf, speed, SAMPLE_RATE) {
                         Ok(processed) => processed,
                         Err(error) => {
                             eprintln!(
